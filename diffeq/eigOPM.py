af4a5a1bb86647e89372fbf823a5c2f3931165c9
Rodrigo Piccinini <BFV8>
2017-11-09 08:47:30 -0200

    compute eigenvalues of OPM-upscale matrix.

---
+++ diffeq/eigOPM.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import loadtxt
+from scipy.sparse import coo_matrix
+from scipy.sparse.linalg import eigsh
+
+print('Reading matrix.')
+i,j,value=loadtxt('./lindat/linsys_mimetic-1-mat.dat').T
+m=coo_matrix((value,(i,j)))
+m=m/m.max()
+#m = m.todense()
+
+print('Computing eigenvalues. It may take a while.')
+w = eigsh(m, k=5, return_eigenvectors=False, which='LM', sigma=1e-12)
+print(w)
+
+#plt.figure()
+#for i in range(m.shape[0]):
+#	for j in  range(m.shape[1]):
+#		if m[i,j] > 0.:
+#			plt.scatter(i,j)
+#
+#plt.savefig('matrix.png')
+# plt.show()
