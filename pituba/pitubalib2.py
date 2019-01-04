b2253e2694055d949b0506d53fdc920988bcac5b
Rodrigo Piccinini <BFV8>
2019-01-04 14:55:04 -0200

    Improvements commenting the library.

--- pituba/pitubalib2.py
+++ pituba/pitubalib2.py
@@ -13,6 +13,8 @@ class classPVT:
         Bubble-point pressure (kgf/cm2).
     co : float
         Undersaturated oil compressibility (cm2/kgf).
+    cw : float
+        Water compressibility (cm2/kgf).
     T : float
         Reservoir fluid temperature (K).
 
@@ -29,17 +31,21 @@ class classPVT:
         Returns gas-oil solubility ratio (sm3/sm3).
     """
 
-    def __init__(self, pvt_table, co, T):
+    def __init__(self, pvt_table, co, cw, T):
         # reading pvt table
-        self.pe = pvt['P']
-        self.Boe = pvt['BO']
-        self.Rse = pvt['RS']
-        self.Bge = pvt['BG']
-        self.Bwe = pvt['BW']
-
+        self.pvt_table = pd.read_excel(pvt_table)
+        self.pe = self.pvt_table['p']
+        self.Boe = self.pvt_table['Bo']
+        self.Rse = self.pvt_table['Rs']
+        self.Bge = self.pvt_table['Bg']
+        self.Bwe = self.pvt_table['Bw']
         # reading other properties
         self.co = co
+        self.cw = cw
         self.T = T
+        # computing gas compressibility factor Z
+        self.Bgi = 1.033/self.pe*(self.T)/288.
+        self.Ze = self.Bge/self.Bgi
 
     def Bo(self, p, Pb):
         """Returns Bo for given bubble-point and oil phase pressure.
@@ -59,10 +65,10 @@ class classPVT:
         """
 
         if p >= Pb:
-            return interp(Pb, self.pe[::-1], self.Boe[::-1]) \
+            return np.interp(Pb, self.pe[::-1], self.Boe[::-1]) \
                     * (1.-self.co*(p-Pb))
         else:
-            return interp(p, self.pe[::-1], self.Boe[::-1])
+            return np.interp(p, self.pe[::-1], self.Boe[::-1])
 
     def Bg(self, p):
         """Returns Bg for given gas phase pressure.
@@ -78,12 +84,8 @@ class classPVT:
             Formation-volume factor of gas phase (m3/sm3).
 
         """
-        if type(self.Ze)==type(1.0):
-            Bgi=1.033/self.pe*(self.T)/293.
-            self.Ze=self.Bge/Bgi
-
-        Z=interp(p,self.pe[::-1],self.Ze[::-1])
-        return 1.033/p*(self.T)/293.*Z
+        Z=np.interp(p,self.pe[::-1],self.Ze[::-1])
+        return 1.033/p*(self.T)/288.*Z
 
     def Bw(self,p):
         """Returns Bw for given gas phase pressure.
@@ -99,7 +101,7 @@ class classPVT:
             Formation-volume factor of water phase (m3/sm3).
 
         """
-        return interp(p,self.pe[::-1],self.Bwe[::-1])
+        return np.interp(p,self.pe[::-1],self.Bwe[::-1])
 
     def Rs(self,p,Pb):
         """Returns Rs for given oil-phase pressure.
@@ -118,25 +120,25 @@ class classPVT:
 
         """
         if p >= Pb:
-            return interp(Pb,self.pe[::-1],self.Rse[::-1])
+            return np.interp(Pb,self.pe[::-1],self.Rse[::-1])
         else:
-            return interp(p,self.pe[::-1],self.Rse[::-1])
+            return np.interp(p,self.pe[::-1],self.Rse[::-1])
 
 
 
 class classAquifer:
-    """Class for aquifer modeling.
+    """Class for aquifer properties and modeling.
 
     Parameters
     ----------
-    WW : float
+    Waq : float
         Aquifer volume (m3).
     p0 : float
         Initial aquifer pressure (kgf/cm2).
-    cw : float
-        water compressbility (cm2/kgf).
     cr : float
         pore volume compressibility (cm2/kgf).
+    pvt : classPVT object
+        Fluid PVT properties.
 
     Attributes
     -------
@@ -144,10 +146,10 @@ class classAquifer:
         Returns aquifer influx for a given final pressure.
 
     """
-    def __init__(self, WW, p0, cw, cr):
-        self.WW = WW
+    def __init__(self, Waq, p0, cr, pvt):
+        self.pvt = pvt
+        self.Waq = Waq
         self.p0 = p0
-        self.cw = cw
         self.cr = cr
 
     def calcWe(self,p):
@@ -167,8 +169,8 @@ class classAquifer:
             Volume of aquifer influx (m3).
 
         """
-        ct=self.cr+self.cw
-        We=ct*self.WW*(self.p0-p)
+        ct=self.cr+self.pvt.cw
+        We=ct*self.Waq*(self.p0-p)
         return We
 
 class classRes:
@@ -187,7 +189,9 @@ class classRes:
     Swcon : float
         Connate water saturation.
     m : float
-        Ratio of initial gas-cap volume to initial oil zone volume
+        Ratio of initial gas-cap volume to initial oil zone volume.
+    pvt : classPVT object
+        Fluid PVT properties.
 
     Attributes
     -------
@@ -223,7 +227,7 @@ class classRes:
         return self.Vp0*(1.-self.cr*(self.p0-p))
 
 class classHist:
-"""Class for loading production, injection and pressure history.
+    """Class for loading production, injection and pressure history.
 
     Parameters
     ----------
@@ -236,237 +240,148 @@ class classHist:
         Returns reservoir pore volume.
 
     """
-    def __init__(self, file):
-        self.data = pd.read_excel(file)
-        self.data =
-
+    def __init__(self, data):
+        self.data = pd.read_excel(data, index_col='Time')
+        self.data[['Np', 'Gp', 'Wp', 'Wi', 'Gi']] = \
+        self.data[['Np', 'Gp', 'Wp', 'Wi', 'Gi']].interpolate(method='index')
+        self.time = self.data.index.values
+        self.p = self.data['p']
+        self.Np = self.data['Np']
+        self.Gp = self.data['Gp']
+        self.Wp = self.data['Wp']
+        self.Wi = self.data['Wi']
+        self.Gi = self.data['Gi']
 
 class classMBE:
-"""Class for defining material balance equation.
+    """Class for defining Material Balance Equation (MBE).
 
     Parameters
     ----------
-    N : float
-        Stock-tank original oil in place - STOOIP (sm3).
-    p0 : float
-        Initial reservoir pressure (kgf/cm2).
-    Pb0 : float
-        Initial bubble-point pressure of oil phase (kgf/cm2).
-    cr : float
-        Pore volume compressibility (cm2/kgf).
-    Swcon : float
-        Connate water saturation.
-    m : float
-        Ratio of initial gas-cap volume to initial oil zone volume
+    hist : object of classHist
+        Production, injection and pressure history.
+    pvt : object of classPVT
+        Fluid PVT properties.
+    res : object of classRes
+        Reservoir properties.
+    aquif : object of classAquifer
+        Aquifer properties.
 
     Attributes
     -------
-    Vp : float
-        Returns reservoir pore volume.
+    xx : xxxx
+        xxxx.
 
     """
+    def __init__(self, hist, pvt, res, aquif):
+        self.hist = hist
+        self.pvt = pvt
+        self.res = res
+        self.aquif = aquif
+        self.out = self.hist.data
 
-    p=0.
-    pold=0.
+    def Eo(self, p, Pb):
+        pvt = self.pvt
+        res = self.res
+        return pvt.Bo(p, Pb)-pvt.Bo(res.p0, res.Pb0) \
+                + (pvt.Rs(res.p0, res.Pb0)\
+                - pvt.Rs(p, Pb))*pvt.Bg(p)
 
-    N=0.
-    Np=0.
-    Gp=0.
+    def Eg(self, p):
+        pvt = self.pvt
+        res = self.res
+        return pvt.Bo(res.p0, res.Pb0)*(pvt.Bg(p)/pvt.Bg(res.p0)-1.)
 
-    Wp=0.
-    Wi=0.
-    We=0.
+    def Efw(self, p):
+        pvt = self.pvt
+        res = self.res
+        m = res.m
+        cr = res.cr
+        cw = pvt.cw
+        return -(1.+m)*pvt.Bo(res.p0,res.Pb0)*(cr+cw*res.Swcon)\
+                *(p-res.p0)/(1.-res.Swcon)
 
-    dt=0.
-
-    aquifero=classAquifero()
-
-    res=classRes()
-    pvt=classPVT()
-    out=classHist()
-
-    petroPhys=classPetrophys()
-
-
-    def Eo(self,p):
-        pvt=self.pvt
-        res=self.res
-        return pvt.Bo(p,pvt.Pb)-pvt.Bo(res.p0,res.Pb0)+(pvt.Rs(res.p0,res.Pb0)-pvt.Rs(p,pvt.Pb))*pvt.Bg(p)
-
-    def Eg(self,p):
-        pvt=self.pvt
-        res=self.res
-        return pvt.Bo(res.p0,res.Pb0)*(pvt.Bg(p)/pvt.Bg(res.p0)-1.)
-
-    def Efw(self,p):
-        pvt=self.pvt
-        res=self.res
-        m=res.m
-        cr=res.cr
-        cw=pvt.cw(p,res.p0)
-        return -(1.+m)*pvt.Bo(res.p0,res.Pb0)*(cr+cw*res.Sw0)*(p-res.p0)/(1.-res.Sw0)
-
-    def Sw(self,p,Wi,Wp,We):
+    def Sw(self, p, Wi, Wp, We):
         res=self.res
         pvt=self.pvt
-        return ((1.+res.m)*self.N*pvt.Bo(res.p0,res.Pb0)*res.Sw0/(1.-res.Sw0)/pvt.Bw(res.p0)+(Wi+We-Wp))*pvt.Bw(p)/res.Vp(p)
+        return ((1.+res.m)*res.N*pvt.Bo(res.p0,res.Pb0)*res.Swcon \
+                /(1.-res.Swcon)/pvt.Bw(res.p0)+(Wi+We-Wp))*pvt.Bw(p)/res.Vp(p)
 
-    def So(self,p,Np):
+    def So(self, p, Pb, Np):
         res=self.res
         pvt=self.pvt
-        return (self.N-Np)/res.Vp(p)*pvt.Bo(p,pvt.Pb)
+        return (res.N-Np)/res.Vp(p)*pvt.Bo(p, Pb)
 
-    def Sg(self,p,N,Np,Gp,Gi):
+    def Sg(self, p, Pb, N, Np, Gp, Gi):
         res=self.res
         pvt=self.pvt
-        return pvt.Bg(p)/res.Vp(p)*(N*((pvt.Rs(res.p0,res.Pb0)-pvt.Rs(p,pvt.Pb))+res.m*pvt.Bo(res.p0,res.Pb0)/pvt.Bg(res.p0))+Gi-Np*(Gp/Np-pvt.Rs(p,pvt.Pb)))
+        return pvt.Bg(p)/res.Vp(p)*(N*((pvt.Rs(res.p0, res.Pb0)-pvt.Rs(p, Pb))\
+                +res.m*pvt.Bo(res.p0, res.Pb0)/pvt.Bg(res.p0)) \
+                +Gi-Np*(Gp/Np-pvt.Rs(p, Pb)))
 
-    def residual(self,p,Np,Gp,Gi,Wi,Wp,dt):
+    def residual(self, p, Pb, Np, Gp, Gi, Wi, Wp):
         res=self.res
         pvt=self.pvt
-        We=self.aquifero.We(p,dt,Np)
+        We=self.aquif.calcWe(p)
         # computing residual:
         # rhs=self.N*(self.Eo(p)+res.m*self.Eg(p)+self.Efw(p))+(Wi+We)*pvt.Bw(p)
         # lhs=Np*(pvt.Bo(p,pvt.Pb)+(Gp/Np-pvt.Rs(p,pvt.Pb))*pvt.Bg(p))+Wp*pvt.Bw(p)
-        E=self.N*(self.Eo(p)+res.m*self.Eg(p)+self.Efw(p))
+        E=res.N*(self.Eo(p, Pb)+res.m*self.Eg(p)+self.Efw(p))
         Fi=(Wi+We)*pvt.Bw(p)+Gi*pvt.Bg(p)
-        Fp=Np*(pvt.Bo(p,pvt.Pb)+(Gp/Np-pvt.Rs(p,pvt.Pb))*pvt.Bg(p))+Wp*pvt.Bw(p)
+        Fp=Np*(pvt.Bo(p, Pb)+(Gp/Np-pvt.Rs(p, Pb))*pvt.Bg(p)) \
+                +Wp*pvt.Bw(p)
         #return (E-Fp+Fi)/(abs(Fp)+abs(Fi)+abs(E))
         return (E-Fp+Fi)/res.Vp0
 
-    def VOIP(self):
-        p=self.res.teste.p
-        Np=self.res.teste.Np
-        Wp=self.res.teste.Wp
-        Wi=self.res.teste.Wi
-        We=zeros(len(Wi))
-        Gi=self.res.teste.Gi
-        Gp=self.res.teste.Gp
-        res=self.res
-        pvt=self.pvt
-        N=[]
-        for i in range(len(p)):
-            E=self.Eo(p[i])+res.m*self.Eg(p[i])+self.Efw(p[i])
-            Fi=(Wi[i]+We[i])*pvt.Bw(p[i])+Gi[i]*pvt.Bg(p[i])
-            Fp=Np[i]*(pvt.Bo(p[i],pvt.Pb)+(Gp[i]/Np[i]-pvt.Rs(p[i],pvt.Pb))*pvt.Bg(p[i]))+Wp[i]*pvt.Bw(p[i])
-            N.append((Fp-Fi)/E)
 
-    def VOIPW(self):
-        p=self.res.teste.p
-        Np=self.res.teste.Np
-        Wp=self.res.teste.Wp
-        Wi=self.res.teste.Wi
-        We=zeros(len(Wi))
-        Gi=self.res.teste.Gi
-        Gp=self.res.teste.Gp
-        res=self.res
-        pvt=self.pvt
-        A=zeros(len(p))
-        B=zeros(len(p))
-        C=zeros(len(p))
-        for i in range(len(p)):
-            A[i]=self.Eo(p[i])+res.m*self.Eg(p[i])+self.Efw(p[i])
-            B[i]=(self.res.cr+self.pvt.cw)*pvt.Bw(p[i])*(self.res.p0-p[i])
-            C[i]=Np[i]*(pvt.Bo(p[i],pvt.Pb)+(Gp[i]/Np[i]-pvt.Rs(p[i],pvt.Pb))*pvt.Bg(p[i]))+Wp[i]*pvt.Bw(p[i])-Wi[i]*pvt.Bw(p[i])-Gi[i]*pvt.Bg(p[i])
-        # least square regression of Ai*N+Bi*We-Ci=0
+    def calcP(self):
+        res = self.res
+        pvt = self.pvt
 
-    def runHist(self,pmin,pmax):
-        out=self.out
-        out.reset()
-        Np=self.Np
-        Gp=self.Gp
-        Gi=self.Gi
-        Wi=self.Wi
-        Wp=self.Wp
-        We=self.We
-        res=self.res
-        pvt=self.pvt
-        dt=self.dt
-
-        px=linspace(pmin, pmax, 100)
-        out.px.append(px/res.Pb0)
-        RGO=gradient(Gp)/gradient(Np)
-
-##        if 'out' in locals():
-##            del out
-##            out=[]
-        for i in range(len(Np)):
-            pvt.Pb=self.updatePb(Np[i],Gp[i],Gi[i])
-#            pbisect,bisect_r=sp.bisect(self.residual, min(pvt.pe[:]) ,0.99*res.p0, args=(Np[i],Gp[i],Wi[i],Wp[i],dt[i]),xtol=1e-5, rtol=1e-6, maxiter=1000, full_output=True, disp=True)
-            pbrentq,brentq_r=sp.brentq(self.residual, min(pvt.pe[:]) ,2.*res.p0, args=(Np[i],Gp[i],Gi[i],Wi[i],Wp[i],dt[i]),xtol=1e-5, rtol=1e-8, maxiter=2000, full_output=True, disp=True)
-#            pnewton=sp.newton(self.residual, pbisect, args=(Np[i],Gp[i],Wi[i],Wp[i],dt[i]),tol=1e-5, maxiter=1000)
-
-            p=pbrentq
-            pnewton=pbrentq
-            fval = self.residual(p,Np[i],Gp[i],Gi[i],Wi[i],Wp[i],dt[i])
-
-
-            if mod(i,2)==0:
-                resd=zeros(len(px))
-                figure(1)
-                for j in range(len(px)):
-                    resd[j]=self.residual(px[j],Np[i],Gp[i],Gi[i],Wi[i],Wp[i],We[i])
-                plot(px/res.Pb0,resd,'k-',linewidth=0.5)
-                plot(pnewton/res.Pb0,self.residual(pnewton,Np[i],Gp[i],Gi[i],Wi[i],Wp[i],We[i]),'sk',markersize=5)
-                plot(pvt.Pb/res.Pb0,self.residual(pvt.Pb,Np[i],Gp[i],Gi[i],Wi[i],Wp[i],We[i]),'ok',markersize=5,markerfacecolor='#ffffff',markeredgewidth=2)
-                xlabel('Pressure [units of $P_{b1}$]')
-                ylabel('Residual Function')
-                grid(1)
-                ylim([-0.1,0.15])
-                xlim([0,2.2])
-
+        # Create dataframe to be returned
+        r = pd.DataFrame(columns=['pCalc', 'Pb', 'So', 'Sg', 'Sw', 'Vp', \
+               'Bo', 'Bg', 'Bw', 'Rs', 'Eo', 'Eg', 'Efw', 'We', 'iter','resd'])
+        r = pd.concat([self.hist.data, r], axis=1)
+        # Solve MBE for pressure
+        for index, row in r.iterrows():
+            Pb = self.updatePb(row['Np'], row['Gp'], row['Gi'])
+            pbrentq,brentq_r=sp.brentq(self.residual, min(pvt.pe[:]), \
+                    max(pvt.pe[:]), args=(Pb, row['Np'], row['Gp'], \
+                    row['Gi'], row['Wi'], row['Wp']), xtol=1e-5, rtol=1e-8, \
+                    maxiter=2000, full_output=True, disp=True)
+            p = pbrentq
+            We = self.aquif.calcWe(p)
 
             # Save output data
-            out.px.append(resd)
-            out.Eo_p1.append(pvt.Bo(p,pvt.Pb)-pvt.Bo(res.p0,res.Pb0))
-            out.Eo_p2.append((pvt.Rs(res.p0,res.Pb0)-pvt.Rs(p,pvt.Pb))*pvt.Bg(p))
-            out.p.append(p)
-            out.Vp.append(res.Vp(p))
-            out.Sw.append(self.Sw(p,Wi[i],Wp[i],self.aquifero.We(p,dt[i],Np[i])))
-            out.So.append(self.So(p,Np[i]))
-            out.Sg.append(self.Sg(p,self.N,Np[i],Gp[i],Gi[i]))
-            out.Rs.append(pvt.Rs(p,pvt.Pb))
-            out.Bo.append(pvt.Bo(p,pvt.Pb))
-            out.residual.append(fval)
-            out.Eo.append(self.Eo(p))
-            out.Eg.append(self.Eg(p))
-            out.Efw.append(self.Efw(p))
-            out.We.append(self.aquifero.We(p,dt[i],Np[i]))
-            out.fw.append(gradient(Wp)[i]/(gradient(Np)[i]+gradient(Wp)[i]))
-            out.Pa.append(self.aquifero.pa(out.We[-1]))
-            out.Fp.append(Np[i]*(pvt.Bo(p,pvt.Pb)+(Gp[i]/Np[i]-pvt.Rs(p,pvt.Pb))*pvt.Bg(p))+Wp[i]*pvt.Bw(p))
-            out.iter.append(brentq_r.iterations)
-
-            # Compute new bubble pressure
-            out.Pb.append(pvt.Pb)
-            print '[iter = ',i,']'
-            print 'brentq iter, fcalls = ',brentq_r.iterations,brentq_r.function_calls
-            print 'p [kgf/cm2], residual = ',p,fval
-            print 'MBE oil = ',out.So[-1]*out.Vp[-1]/pvt.Bo(p,pvt.Pb)/(self.N-Np[i])
-            print 'Pb [kgf/cm2] = ',out.Pb[-1]
-            print 'RGO = ', RGO[i]
-            print 'FR Oil, FR Gas = ',Np[i]/self.N, Gp[i]/(self.N*pvt.Rs(res.p0,res.Pb0))
-            print 'So, Sg, Sw = ',out.So[-1],out.Sg[-1],out.Sw[-1]
-            print '--------------------------------------------'
-
-        plot(pnewton/res.Pb0,self.residual(pnewton,Np[i],Gp[i],Gi[i],Wi[i],Wp[i],We[i]),'sk',markersize=5,label='solution')
-        plot(pvt.Pb/res.Pb0,self.residual(pvt.Pb,Np[i],Gp[i],Gi[i],Wi[i],Wp[i],We[i]),'ok',markersize=5,markerfacecolor='#ffffff',markeredgewidth=2,label='bubble-point pressure')
-        legend(loc='upper right')
-        subplots_adjust(bottom=0.15,left=0.15)
-        return out
+            r.loc[index, 'pCalc'] = pbrentq
+            r.loc[index, 'Pb'] = Pb
+            r.loc[index, 'resd'] = self.residual(p, Pb, row['Np'],
+                    row['Gp'], row['Gi'], row['Wi'], row['Wp'])
+            r.loc[index, 'iter'] = brentq_r.iterations
+            r.loc[index, 'Vp'] = res.Vp(p)
+            r.loc[index, 'We'] = self.aquif.calcWe(p)
+            r.loc[index, 'Sw'] = self.Sw(p, row['Wi'], row['Wp'], We)
+            r.loc[index, 'So'] = self.So(p, Pb, row['Np'])
+            r.loc[index, 'Sg'] = self.Sg(p, Pb, res.N, row['Np'], \
+                    row['Gp'], row['Gi'])
+            r.loc[index, 'Rs'] = pvt.Rs(p, Pb)
+            r.loc[index, 'Bo'] = pvt.Bo(p, Pb)
+            r.loc[index, 'Eo'] = self.Eo(p, Pb)
+            r.loc[index, 'Eg'] = self.Eg(p)
+        return r
 
     def EqPb(self,Np,Gp,Gi):
         res=self.res
         pvt=self.pvt
         pmax=max(pvt.pe[:])
-        return (self.N*(pvt.Rs(res.p0,res.Pb0)+res.m*pvt.Bo(res.p0,res.Pb0)/pvt.Bg(res.p0))-Gp+Gi)/(self.N-Np)
+        return (res.N*(pvt.Rs(res.p0,res.Pb0) \
+                +res.m*pvt.Bo(res.p0,res.Pb0)/pvt.Bg(res.p0))-Gp+Gi)/(res.N-Np)
 
     def updatePb(self,Np,Gp,Gi):
         res=self.res
         pvt=self.pvt
         RsPb=self.EqPb(Np,Gp,Gi)
-        r=interp(RsPb,pvt.Rse[::-1],pvt.pe[::-1])
-        r=max(r,pvt.pe[-2])
+        r=np.interp(RsPb,pvt.Rse[::-1],pvt.pe[::-1])
+#        r=max(r,pvt.pe[-2])
         return r
 
 #------------------------------------------------------------------------
@@ -487,7 +402,7 @@ class classMBE:
         GpOld=0.
         for i in range(len(teste.p)):
             p=teste.p[i]
-            t=interp(Np[i],self.Np,self.dt)
+            t=np.interp(Np[i],self.Np,self.dt)
             We=self.aquifero.We(p,t,Np[i])
             F=self.N*(self.Eo(p)+res.m*self.Eg(p)+self.Efw(p))+(Wi[i]+We)*pvt.Bw(p)
             Gp=pvt.Rs(p,pvt.Pb)*Np[i]-pvt.Bo(p,pvt.Pb)/pvt.Bg(p)*Np[i]-pvt.Bw(p)/pvt.Bg(p)*Wp[i]+F/pvt.Bg(p)
@@ -552,7 +467,7 @@ class classMBE:
 ##            out=[]
         for i in range(len(Np)):
             pvt.Pb=self.updatePb(Np[i],Gp[i],Gi[i])
-            print 'Residual = ', self.We_residual(0.,p[i],Np[i],Gp[i],Gi[i],Wi[i],Wp[i])
+            # print 'Residual = ', self.We_residual(0.,p[i],Np[i],Gp[i],Gi[i],Wi[i],Wp[i])
 #            We[i],brentq_r=sp.bisect(self.We_residual, 0. ,10.*(Wp[i]+Np[i]), args=(p[i],Np[i],Gp[i],Gi[i],Wi[i],Wp[i]),xtol=1e-8, rtol=1e-8, maxiter=2000, full_output=True, disp=True)
             We[i],brentq_r=sp.brentq(self.We_residual, 0. ,5.*(Wp[i]+Np[i]), args=(p[i],Np[i],Gp[i],Gi[i],Wi[i],Wp[i]),xtol=1e-8, rtol=1e-8, maxiter=2000, full_output=True, disp=True)
             fval = self.We_residual(We[i],p[i],Np[i],Gp[i],Gi[i],Wi[i],Wp[i])
@@ -572,15 +487,15 @@ class classMBE:
 
             # Compute new bubble pressure
             out.Pb.append(pvt.Pb)
-            print '[iter = ',i,']'
-            print 'brentq iter, fcalls = ',brentq_r.iterations,brentq_r.function_calls
-            print 'p [kgf/cm2], residual = ',p[i],fval
-            print 'We [MM m3] = ', We[i]/1e6
-            print 'MBE oil = ',out.So[-1]*out.Vp[-1]/pvt.Bo(p[i],pvt.Pb)/(self.N-Np[i])
-            print 'Pb [kgf/cm2] = ',out.Pb[-1]
-            print 'RGO = ', RGO[i]
-            print 'FR Oil, FR Gas = ',Np[i]/self.N, Gp[i]/(self.N*pvt.Rs(res.p0,res.Pb0))
-            print 'So, Sg, Sw = ',out.So[-1],out.Sg[-1],out.Sw[-1]
-            print '--------------------------------------------'
+#             print '[iter = ',i,']'
+#             print 'brentq iter, fcalls = ',brentq_r.iterations,brentq_r.function_calls
+#             print 'p [kgf/cm2], residual = ',p[i],fval
+#             print 'We [MM m3] = ', We[i]/1e6
+#             print 'MBE oil = ',out.So[-1]*out.Vp[-1]/pvt.Bo(p[i],pvt.Pb)/(self.N-Np[i])
+#             print 'Pb [kgf/cm2] = ',out.Pb[-1]
+#             print 'RGO = ', RGO[i]
+#             print 'FR Oil, FR Gas = ',Np[i]/self.N, Gp[i]/(self.N*pvt.Rs(res.p0,res.Pb0))
+#             print 'So, Sg, Sw = ',out.So[-1],out.Sg[-1],out.Sw[-1]
+#             print '--------------------------------------------'
 
         return out
