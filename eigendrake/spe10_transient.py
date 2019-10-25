59e8a2f16de192177a4cd11a6f381c56396bebe7
Rodrigo <rbpiccinini@gmail.com>
2019-10-25 01:11:07 -0300

    cool

--- eigendrake/spe10_transient.py
+++ eigendrake/spe10_transient.py
@@ -4,6 +4,7 @@ import numpy as np
 from slepc4py import SLEPc
 from firedrake.petsc import PETSc
 import scipy.io as sp
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -21,9 +22,15 @@ def coords2ijk(x, y, z, Delta, data_array):
     return data_array[i, j, k]
 
 # Define time steps
-T = 20.0            # final time
-num_steps = 50     # number of time steps
-dt = T / num_steps # time step size
+tp = 3. # prod time in hours
+t = np.linspace(0.,10*24*3600, 480)
+#t = list(np.logspace(np.log10(0.01*3600), np.log10(tp*3600), 80))
+#t = t+ list(tp*3600+np.logspace(np.log10(0.01*3600), np.log10(20.*24*3600), 150))
+#t = np.array(t)
+
+
+dts = np.diff(t)
+dt = fd.Constant(dts[0]) # time step size
 
 # Define mesh
 Delta_x = 20*0.3048 # ft -> m
@@ -33,7 +40,7 @@ Delta_z = 2*0.3048
 Nx = int(60)
 Ny = int(220)
 
-layers = range(35,40)
+layers = range(5) # range(35,40)
 Nz = len(layers)
 
 Lx = Nx*Delta_x
@@ -105,14 +112,14 @@ Kz.dat.data[...] = coords2ijk(coords[:, 0], coords[:, 1],
 phi.dat.data[...] = coords2ijk(coords[:, 0], coords[:, 1],
                                        coords[:, 2], Delta=Delta, data_array=phi_array)
 Tx.dat.data[...] = coords2ijk(coords[:, 0], coords[:, 1],
-                                    coords[:, 2], Delta=Delta, data_array=kx_array/mu*24*3600)
+                                    coords[:, 2], Delta=Delta, data_array=kx_array/mu)
 Ty.dat.data[...] = coords2ijk(coords[:, 0], coords[:, 1],
-                                    coords[:, 2], Delta=Delta, data_array=ky_array/mu*24*3600)
+                                    coords[:, 2], Delta=Delta, data_array=ky_array/mu)
 Tz.dat.data[...] = coords2ijk(coords[:, 0], coords[:, 1],
-                                    coords[:, 2], Delta=Delta, data_array=kz_array/mu*24*3600)
+                                    coords[:, 2], Delta=Delta, data_array=kz_array/mu)
 
 w.dat.data[...] = coords2ijk(coords[:, 0], coords[:, 1],
-                                       coords[:, 2], Delta=Delta, data_array=(phi_array+1e-4)*ct)
+                                       coords[:, 2], Delta=Delta, data_array=(phi_array+1e-6)*ct)
 print("END: Read in reservoir fields")
 
 # Define initial value
@@ -123,7 +130,8 @@ yc = 0.5*Delta_y*Ly
 r = ((x-xc)**2+(y-yc)**2)**0.5 
 
 # u_0 = fd.Function(V).interpolate(10*y/(Delta_y*Ny))
-u_0 = fd.Function(V).interpolate(fd.Constant(0.))
+pi = 400e5
+u_0 = fd.Function(V).interpolate(fd.Constant(pi)) # 400 bar
 u_0.rename('Pressure')
 
 u_n = fd.Function(V).assign(u_0)
@@ -145,19 +153,25 @@ Tz_facet = fd.conditional(fd.gt(fd.avg(Tz), 0.0), Tz('+')*Tz('-') / fd.avg(Tz), 
 
 # Define the well source term
 #f = fd.Function(V).interpolate(10*fd.exp(-r**2/eps)/eps**0.5)
-
 xw = Delta_x*Nx*0.5
 yw = Delta_y*Ny*0.5
+
+# line = np.array([xw, yw, 1])*np.ones([10,3])
+# line[:, 2] = np.linspace(0., Lz, len(line)) 
+
 radius = 0.1 #0.1875*0.3048 # 0.1 radius of well # 0.1875*0.3048 from ChenZhang2009
 f = fd.Function(V)
-f.assign(fd.interpolate(fd.conditional(pow(x-xw,2)+pow(y-yw,2)<pow(radius,2), fd.exp(-(1.0/(-pow(x-xw,2)-pow(y-yw,2)+pow(radius,2)))), 0.0), V))
+f.assign(fd.interpolate(fd.conditional(pow(x-xw,2)+pow(y-yw,2)<pow(radius,2), fd.exp(-(1.0/(-pow(x-xw,2)-pow(y-yw,2)+pow(radius,2))))*Kx, 0.0), V))
 norm = fd.assemble(f*fd.dx)
-f.assign(1e-4*f/norm)
+
+pv = fd.assemble(phi*fd.dx) # pore volume
+q = -0.1*pv/(30*3600*24) # prod rate of 0.01*pv / month
+f.assign(q*f/norm)
 
 # Plot source
-rr = np.logspace(-5, np.log(Delta_x*Nx),100)
-ff = np.exp(-rr**2/eps)/eps**0.5
-plt.loglog(rr, ff)
+# rr = np.logspace(-5, np.log(Delta_x*Nx),100)
+# ff = np.exp(-rr**2/eps)/eps**0.5
+# plt.loglog(rr, ff)
 # plt.show()
 
 # We can now define the bilinear and linear forms for the left and right
@@ -173,26 +187,54 @@ L = fd.rhs(F)
 # m = u * v * w * dx
 
 u = fd.Function(V)
-t = 0
 print('Start solver')
-pavg0 = fd.assemble(u_0*dx)/vol
-print("t= {:6.3f}  \t pavg = {:1.4e}".format(t, pavg0))
 
+t = []
 pwf = []
-pwf.append([t, u_0.at([xw, yw, 0])])
-for n in range(num_steps):
+pavg = []
+q = []
+
+# t =0
+t.append(0.)
+pwf.append(u_0.at([xw, yw, 0]))
+pavg.append(pi)
+q.append(0)
+
+print("t [h] = {:6.3f}  \t pavg [bar] = {:6.3f}".format(t[-1]/3600, pavg[-1]/1e5))
+shutin = False
+for n in range(len(dts)):
     fd.solve(a == L, u, solver_parameters={'ksp_type': 'cg'})
     
     # Update previous solution
     u_n.assign(u)
-    t += dt
+    dt.assign(fd.Constant(dts[n]))
     
-    # Save to file and plot solution
-    # if n % 20 == 0:
-    outfile.write(u_n)
-    pwf.append([t, u_n.at([xw, yw, 0])])
-    pavg = fd.assemble(u_n*dx)/vol
-    print("t= {:6.3f}  \t pavg = {:1.4e}".format(t, pavg))
+    # Save t, pwf and pavg to list
+    t.append(t[-1] + dts[n])
+    pwf.append(u_n.at([xw, yw, 0]))
+    pavg.append(fd.assemble(u_n*dx)/vol)
+    q.append((pavg[-1]-pavg[-2])*ct*pv/dts[n])
+    
+    # Update source term 
+    if t[-1]>=tp*3600 and not shutin:
+        f.assign(fd.Constant(0))
+        shutin=True
+        print('well shutin')
 
+    # Save to VTK file
+    if n % 10 == 0:
+        outfile.write(u_n)
 
-pwf = np.array(pwf)
+    # print info 
+    print("t [h] = {:6.3f}  \t pavg [bar] = {:6.3f} \t pwf = {:6.3f} \t q [m3/d] = {:4.1f}".format(t[-1]/3600, pavg[-1]/1e5, pwf[-1]/1e5, q[-1]*24*3600))
+
+    if abs(pwf[-1]-pavg[-1]) < 1.:
+        break
+
+# defining dataframe for results
+df = pd.DataFrame(columns=['t (h)', 'pwf (bar)','pavg (bar)', 'q (m3/d)'])
+df['t (h)'] = np.array(t)*3600
+df['pwf (bar)'] = np.array(pwf)*1e5
+df['pavg (bar)'] = np.array(pavg)*1e5
+df['q (m3/d)'] = np.array(q)*24*3600
+df.to_csv(solution.dat)
